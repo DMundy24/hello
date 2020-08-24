1bc249d9062afe2d109a736dbb8b81c96b3227d0
chunter24 <69820810+chunter24@users.noreply.github.com>
2020-08-24 17:12:10 -0500

    Added Name & Major

--- hello.c
+++ hello.c
@@ -11,7 +11,8 @@ int main(int argc, char **argv) {
 
     printf("Dillon Mundy!\n");
     printf("Computer Engineering\n");
-
+    printf("Carrell Hunter!\n");
+    printf("Computer Science\n");
     return 0;
 
 }
