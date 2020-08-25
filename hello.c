665ff84ce26c1b1f1dedc218886cfae0ee48cdb8
chunter24 <chunter19@huskers.unl.edu>
2020-08-25 00:39:27 +0000

    removed line

--- hello.c
+++ hello.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv) {
     printf("Computer Engineering\n");
     printf("Carrell Hunter!\n");
     printf("Computer Science\n");
-    printf("Carrell is stupid\n");
+    
     return 0;
 
 }
